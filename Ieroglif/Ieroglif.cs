65e1cb5e58317f06295fe4166a9343117aa07d11
DESKTOP-CLPATVJ\CFYZXIII <cfyzxiii@gmail.com>
2021-04-23 16:18:15 +0400

    no message

---
+++ Ieroglif/Ieroglif.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ieroglif
+{
+    class Ieroglif
+    {
+        //возможные слоги иероглифа
+       public String[] slog;
+
+        //номер в массиве
+        public int index;
+
+        public Ieroglif nextieroglif;
+
+     
+        public Ieroglif(String[] slog)
+        {
+            this.slog = slog;
+        }
+
+        
+        public void variants(String s) 
+        {
+            for (int i = 0; i < slog.Length; i++) 
+            {
+                //если есть следующий, то кидаем на вход метода всё что накопили
+                if (nextieroglif != null)
+                {
+                    
+                    nextieroglif.variants(s+slog[i]);
+                  
+                }
+                else
+                    Console.WriteLine(s+slog[i]);
+
+            }
+           
+            
+        }
+
+        public void GetAllVariants(String s, List<string> strings)
+        {
+            for (int i = 0; i < slog.Length; i++)
+            {
+                //если есть следующий, то кидаем на вход метода всё что накопили
+                if (nextieroglif != null)
+                {
+
+                    nextieroglif.GetAllVariants(s + slog[i], strings);
+
+                }
+                else
+                {
+                    strings.Add(s + slog[i]);
+                }
+
+            }
+
+
+        }
+
+
+    }
+
+    class IeroglifList 
+    {
+        private List<Ieroglif> ierogliflList = new List<Ieroglif>();
+
+        public void Add(Ieroglif ieroglif)
+        {
+            this.ierogliflList.Add(ieroglif);
+            int index = ieroglif.index = ierogliflList.IndexOf(ieroglif);
+
+            if (ierogliflList.Count > 1)
+            {
+                //записываем в предыдущий иероглиф ссылку на текущий иероглиф
+                //соответственно у последнего будет null
+                ierogliflList[index - 1].nextieroglif = ieroglif;
+            }
+           
+        }
+
+        public Ieroglif Get(int i)
+        {
+            return this.ierogliflList[i];
+        }
+
+        
+
+       
+    }
+}
---
+++ Ieroglif/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ieroglif
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //создаём иероглифы
+           Ieroglif a = new Ieroglif(new string[] { "sin", "zin", "pin","loh" });
+           Ieroglif b = new Ieroglif(new string[] { "sun", "hun", "kan" });
+           Ieroglif c = new Ieroglif(new string[] { "hao", "mao", "lao" });
+
+            //словарь иероглифов
+            Dictionary<string, Ieroglif> ieroglifs = new Dictionary<string, Ieroglif>();
+
+            ieroglifs.Add("a", a);
+            ieroglifs.Add("b", b);
+            ieroglifs.Add("c", c);
+
+            //Список иероглифов которые будут заполняться
+            IeroglifList ieroglifList = new IeroglifList();
+
+            //приходит на вход строка...
+            String s = "abc";
+
+            
+            //идём по строке и ищем наши иероглифы
+            for (int i = 0; i < s.Length; i++)
+            {
+                ieroglifList.Add(ieroglifs[s[i].ToString()]); // считаю всё в string,  если захочешь потом переделаешь в char
+            }
+
+            //вывод полученных комбинаций
+           
+
+            //список возможных комбинаций
+            List<string> strings = new List<string>();
+ 
+            //перебираем комбинации...
+            ieroglifList.Get(0).GetAllVariants(string.Empty,strings);
+
+            foreach (string l in strings)
+            {
+                Console.WriteLine("комбинация "+strings.IndexOf(l)+" из "+strings.Count+" " + l);
+            }
+
+            //дальше с полученным списком комбинаций можно делать что угодно
+            
+        }
+    }
+}
